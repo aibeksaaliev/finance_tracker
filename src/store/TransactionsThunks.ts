2a6c2a01b6d2ae3349ee79ab1d7f818f31a3721b
Aibek Saaliev <dokaidea@gmail.com>
2023-01-12 18:14:06 +0600

    sorted extended transactions by date from newest to oldest

--- src/store/TransactionsThunks.ts
+++ src/store/TransactionsThunks.ts
@@ -37,7 +37,9 @@ export const fetchAllTransactions = createAsyncThunk<ExtendedTransactionType[], 
         })
       })
 
-      return extendedTransactions.reverse();
+      return extendedTransactions.sort((a, b) => {
+        return new Date(a.createdAt) < new Date(b.createdAt) ? 1 : -1;
+      });
     }
 
     return [];
